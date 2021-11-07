4bf0fc4b7567301ab802c4810500e742ca2611a4
sergisanchezprieto <33113666+sergisanchezprieto@users.noreply.github.com>
2021-11-07 17:03:06 +0100

    Update admin.py
    Cambios

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Receta, Ingrediente, Cantidad
 
+admin.site.register(Receta)
+admin.site.register(Ingrediente)
+admin.site.register(Cantidad)
 # Register your models here.
