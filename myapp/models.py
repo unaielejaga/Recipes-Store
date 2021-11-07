3179c4cb76e35694dcc724b22ec8f4ad350465a5
Unai Elejaga Jimeno <unai.elejaga@opendeusto.es>
2021-11-07 16:51:41 +0100

    Modelo Recetas

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+class TipoReceta(Enum):
+    D = "Desayuno"
+    A = "Aperitivo"
+    CO = "Comida"
+    M = "Merienda"
+    CE = "Cena"
+
+class DuracionReceta(Enum):
+    MR = "Muy rapido"
+    R = "Rapido"
+    M = "Medio"
+    L = "Lento"
+    ML = "Muy lento"
+
+class Receta(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=1000)
+    tiempo = models.IntegerField()
+    tipo = models.CharField(
+        choices=[(tag, tag.value) for tag in TipoReceta]
+    )
+    duracion = models.CharField(
+        choices=[(tag, tag.value) for tag in DuracionReceta]
+    )
+    def __str__(self):
+        return self.nombre
+
+
