a0c97bd0633d70c403097863280a386bc0ffcfbf
sergisanchezprieto <33113666+sergisanchezprieto@users.noreply.github.com>
2021-11-07 16:49:16 +0100

    Update models.py
    clase ingrediente

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# Creacion de la clase Ingrediente
+class Ingrediente(models.Model):
+    nombre = models.CharField(max_length=20)
+    calorias = models.IntegerField
+    receta = ForeignKey(Receta, on_delete=models.CASCADE)
+    cantidad = ForeignKey(Cantidad, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
