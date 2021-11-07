4d6078c00a5125302cc85b8626c21c052ac3c8b4
sergisanchezprieto <33113666+sergisanchezprieto@users.noreply.github.com>
2021-11-07 19:09:28 +0100

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from .models import Receta, Ingrediente, Cantidad
+from django.http import HttpResponse
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.db.models.query import QuerySet
 # Create your views here.
+
+# Vista para ver todos los ingredientes
+
+class IngredientesListView(ListView):
+    model = Ingrediente
+    querySet = Ingrediente.objects.all()
+    template_name = 'listaIngredientes.html'
+
+class IngredientesDetailView(DetailView):
+    model = Ingrediente
+    template_name = 'detalleIngrediente.html'
