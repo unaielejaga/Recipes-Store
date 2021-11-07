0ec5588cfb70f341db09fab6ba0d6f5ddda7ca55
sergisanchezprieto <33113666+sergisanchezprieto@users.noreply.github.com>
2021-11-07 16:55:50 +0100

    Update models.py
    cambios ingrediente

--- myapp/models.py
+++ myapp/models.py
@@ -32,9 +32,9 @@ class Receta(models.Model):
 # Creacion de la clase Ingrediente
 class Ingrediente(models.Model):
     nombre = models.CharField(max_length=20)
-    calorias = models.IntegerField
-    receta = ForeignKey(Receta, on_delete=models.CASCADE)
-    cantidad = ForeignKey(Cantidad, on_delete=models.CASCADE)
+    calorias = models.IntegerField()
+    receta = models.ForeignKey(Receta, on_delete=models.CASCADE)
+    cantidad = models.ForeignKey(Cantidad, on_delete=models.CASCADE)
     def __str__(self):
         return self.nombre
 
