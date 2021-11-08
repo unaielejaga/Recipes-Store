2b5ed60c9eac812b536c416f9aa001d0ee730d41
Irati98o <irati98o@hotmail.es>
2021-11-08 21:46:44 +0100

    Update urls.py
    añadido de paths

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import IngredientesDetailView, IngredientesListView, RecetasDetailView, RecetasListView, TiposListView
+from .views import IngredientesDetailView, IngredientesListView, RecetasDetailView, RecetasListView, TiposListView, TiposDetailView
 
 urlpatterns = [
     # Lista ingredientes
@@ -10,6 +10,7 @@ urlpatterns = [
     path('ingredientes/<int:pk>', IngredientesDetailView.as_view(), name='id_ing'),
     
     path('tipos/', TiposListView.as_view()),
+    path('tipos/<int:pk>', TiposDetailView.as_view(), name = 'id_tip'),
 
     path('recetas/', RecetasListView.as_view()),
     path('recetas/<int:pk>', RecetasDetailView.as_view(), name = 'id_re'),
