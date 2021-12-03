5c7377afdf11fbdad764a0b26352d2bf97451f41
Unai Elejaga Jimeno <unai.elejaga@opendeusto.es>
2021-12-03 08:36:48 +0100

    Formulario y barra bonitos

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.forms.widgets import PasswordInput
 
 
 class MyForm(forms.Form):
     nombre = forms.CharField(label = 'Introduce tu nombre', max_length = 100)
     email = forms.EmailField(label = 'Introduce tu email', max_length = 100)
+    password = forms.CharField(label = 'Introduce contraseña', widget=PasswordInput(), max_length=100)
