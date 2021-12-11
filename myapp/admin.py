0d2f72774c162258f5ce04657d600d87cfa6a841
sergisanchezprieto <33113666+sergisanchezprieto@users.noreply.github.com>
2021-12-11 17:47:03 +0100

    cambios admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
 from .models import DuracionReceta, Receta, Ingrediente, Cantidad, TipoReceta, Usuario
 
-admin.site.register(Receta)
-admin.site.register(Ingrediente)
+
+
+class CantidadInline(admin.TabularInline):
+    model = Cantidad
+    extra = 1
+
+class RecetaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Resumen',               {'fields': ['nombre', 'descripcion']}),
+        ('Tipo de receta',        {'fields': ['tipo']}),
+        ('Tiempo y duracion',     {'fields': ['tiempo', 'duracion'], 'classes': ['collapse']}),
+    ]
+    inlines = [CantidadInline]
+    list_display = ('nombre', 'tipo', 'duracion')
+
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'calorias')
+
+
+
+admin.site.register(Receta, RecetaAdmin)
+admin.site.register(Ingrediente, IngredienteAdmin)
 admin.site.register(Cantidad)
 admin.site.register(TipoReceta)
 admin.site.register(DuracionReceta)
 admin.site.register(Usuario)
 # Register your models here.
+
+
+
+
